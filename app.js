8433a68f02805c14685adb5af09a9bd37e37b42c
NikuDev <niek_kuijken@hotmail.com>
2020-01-09 11:03:06 +0100

    - added 1 extra model - updated html to include table - started using app.js instead of inline script
    Signed-off-by: NikuDev <niek_kuijken@hotmail.com>

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ var viewModelTask = {
 	createdDate: "08-Dec-2019"
 }
 
+var personModel = {
+	ID: 0,
+	firstName: "Bill",
+	lastName: "Hickok",
+	age: 46
+}
+
 ko.applyBindings(viewModelTask);
+ko.applyBindings(personModel);
