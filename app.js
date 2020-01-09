f09f32107d465719917936a158a6679bbbc17d13
NikuDev <niek_kuijken@hotmail.com>
2020-01-09 12:25:47 +0100

    - added a 'ko.computed' property with separate read/write functions (get/set)
    Signed-off-by: NikuDev <niek_kuijken@hotmail.com>

--- app.js
+++ app.js
@@ -7,10 +7,38 @@ var viewModelTask = {
 var viewModelPerson = {
 	ID: ko.observable(0),
 	firstName: ko.observable("Bill"),
-	lastName: ko.observable("Hickok"),
+	lastName: ko.observable("Murray"),
 	age: ko.observable(46)
 }
 
-ko.applyBindings();
+var viewModelComputedPropPerson = function()
+{
+	firstName= ko.observable("Hank"),
+	lastName= ko.observable("Schroder"),
+	// knockout 'computed' functions
+	fullName= ko.computed({
+		read: function(){
+			return this.firstName() + " " + this.lastName();
+		},
+		// just to show 'read' and 'write' can be distinctly
+		// called within a computed prop (get set)
+		write: function(val){
+			var fullName = val.split(' ');
+			this.firstName(fullName[0]);
+			this.lastName(fullName[1]);
+		}
+	});
+}
 
-viewModelPerson.firstName('Wild Bill');
+ko.applyBindings(viewModelComputedPropPerson);
+
+// 2 seconds after the document loaded the name will be changed
+// (and automatically updated in the view due to knockout)
+setTimeout(function() {
+	// to read:
+	viewModelPerson.firstName();
+	// to write:
+	viewModelPerson.firstName('Wild Bill');
+	// or
+	viewModelPerson.firstName('Wild Bill').lastName('Hickok');
+  }, 2000);
